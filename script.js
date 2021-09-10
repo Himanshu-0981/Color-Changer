e8a484dd594b19d199ee224df0951823b37126f5
Himanshu <kumarhimanshu2219@gmail.com>
2021-09-10 18:56:41 +0530

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 
-            function changeBg(color){
-                document.body.style.backgroundColor = color;
+        let output = document.getElementById("output-box");
+        let colorOutputName = document.getElementById("color-output-name");
+        let colorOutputCode = document.getElementById("color-output-code");
 
-                let txt = document.getElementsByClassName('text');
+        function colorChange(color) {
+            output.style.backgroundColor = color;
 
-                if(color == '#000000'){
-                    document.body.style.color = "#ffffff";
-                    document.body.style.transition = '0.5s';
-                    // document.body.style.b = "1px solid #ffffff";
-                }else{
-                    document.body.style.color = "#000000";
-                }
+            if (color == "#fffdd0") {
+                colorOutputName.innerHTML = "you choosen cream color";
+                colorOutputCode.innerHTML = "#FFFDD0";
+            } else if (color == "#00ff00") {
+                colorOutputName.innerHTML = "you choosen green color ";
+                colorOutputCode.innerHTML = "#00FF00";
+            } else if (color == "#FFFF00") {
+                colorOutputName.innerHTML = "you choosen yellow color";
+                colorOutputCode.innerHTML = "#FFFF00";
+            } else if (color == "#00FFFF") {
+                colorOutputName.innerHTML = "you choosen aqua color";
+                colorOutputCode.innerHTML = "#00FFFF";
+            } else {
+                colorOutputName.innerHTML = "you choosen black color";
+                colorOutputCode.innerHTML = "#000000";
             }
+        }
